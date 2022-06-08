3dbe2e83c131336897922ba32381599837019a70
humen <humen@humen.com>
2022-06-08 15:45:38 +0800

    master push1

--- src/main/java/com/study/git/Main.java
+++ src/main/java/com/study/git/Main.java
@@ -8,6 +8,7 @@ public class Main {
         System.out.println("git3");
         System.out.println("git host-fix git1");
         System.out.println("git host-fix test");
+        System.out.println("push test");
     }
 
 }
