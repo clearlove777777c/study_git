e4fdc0d8fdc33184fb6d2f0d6570dda1dbd30334
humen <humen@humen.com>
2022-06-08 13:07:13 +0800

    master v3

--- src/main/java/com/study/git/Main.java
+++ src/main/java/com/study/git/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args){
         System.out.println("git1");
         System.out.println("git2");
+        System.out.println("git3");
     }
 
 }
